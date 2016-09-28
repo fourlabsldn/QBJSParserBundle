2aa3590d6d6176da948d0c664abb340a9a00c3b8
Luis Galeas <galeaspablo@hotmail.com>
2016-09-28 16:19:32 +0100

    validation between builders and mappings

--- Service/BuildersService.php
+++ Service/BuildersService.php
@@ -12,34 +12,64 @@ class BuildersService
     private $builders;
 
     /**
-     * @var QBJSDoctrineParserService
-     */
-    private $qbjsParser;
-
-    /**
      * ParserQueryService constructor.
-     * @param array $builders
-     * @param QBJSDoctrineParserService $qbjsParser
+     * @param array $buildersConfig
+     * @param array $classesAndMappings
      */
-    public function __construct(array $builders, QBJSDoctrineParserService $qbjsParser)
+    public function __construct(array $buildersConfig, array $classesAndMappings)
     {
-        foreach ($builders as $builderId => $builder) {
-            $builder['id'] = $builderId;
-            $parserQuery = new Builder();
-            $parserQuery
-                ->setClassName($builder['class'])
-                ->setHumanReadableName($builder['human_readable_name'])
+        $this->validate($buildersConfig, $classesAndMappings);
+        foreach ($buildersConfig as $builderId => $builderConfig) {
+            $builderConfig['id'] = $builderId;
+            $builder = new Builder();
+            $builder
+                ->setClassName($builderConfig['class'])
+                ->setHumanReadableName($builderConfig['human_readable_name'])
             ;
-            unset($builder['class']);
-            unset($builder['human_readable_name']);
-            $parserQuery->setJsonString(json_encode($builder));
-            $this->builders[] = $parserQuery;
+            unset($builderConfig['class']);
+            unset($builderConfig['human_readable_name']);
+            $builder->setJsonString(json_encode($builderConfig));
+            $this->builders[] = $builder;
         }
     }
 
-    private function validateAgainstParser()
+    /**
+     * @param array $buildersConfig
+     * @param array $classesAndMappings
+     */
+    private function validate(array $buildersConfig, array $classesAndMappings)
     {
-
+        foreach ($buildersConfig as $builderId => $config) {
+            if (! array_key_exists('class', $config)) {
+                throw new \InvalidArgumentException(sprintf(
+                    'Builders Configuration: Expected a class in builder with ID %s',
+                    $builderId
+                ));
+            }
+            $builderClass = $config['class'];
+            if (! class_exists($builderClass)) {
+                throw new \InvalidArgumentException(sprintf(
+                    'Builders Configuration: %s is not a valid class in builder with ID %s ',
+                    $builderClass,
+                    $builderId
+                ));
+            }
+            foreach($classesAndMappings as $classAndMapping){
+                $mappingClass = $classAndMapping['class'];
+                $mappingProperties = $classAndMapping['properties'];
+                if($builderClass === $mappingClass){
+                    foreach($config['filters'] as $filter){
+                        if (! array_key_exists($filter['id'], $mappingProperties) ){
+                            throw new \InvalidArgumentException(sprintf(
+                                'Builders Configuration: Invalid Mapping for filter with ID %s, in builder with ID %s ',
+                                $filter['id'],
+                                $builderId
+                            ));
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /**
