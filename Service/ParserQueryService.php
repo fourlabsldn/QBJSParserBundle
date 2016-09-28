dd510b9601db275e2b59b863d8e98ad6d4712982
Luis Galeas <galeaspablo@hotmail.com>
2016-09-28 09:38:38 +0100

    parserQueryService

--- Service/ParserQueryService.php
+++ Service/ParserQueryService.php
@@ -6,8 +6,18 @@ use FL\QBJSParserBundle\Model\ParserQuery;
 
 class ParserQueryService
 {
+    /**
+     * @var ParserQuery[]
+     */
+    private $parserQueries;
+
+    /**
+     * ParserQueryService constructor.
+     * @param array $queryGenerators
+     */
     public function __construct(array $queryGenerators)
     {
+        $parserQueries = [];
         foreach ($queryGenerators as $generatorId => $generator) {
             $generator['id'] = $generatorId;
             $parserQuery = new ParserQuery();
@@ -16,5 +26,13 @@ class ParserQueryService
             $parserQuery->setJsonString(json_encode($generator));
             $parserQueries[] = $parserQuery;
         }
+    }
+
+    /**
+     * @return ParserQuery[]
+     */
+    public function getParserQueries()
+    {
+        return $this->parserQueries;
     }
 }
