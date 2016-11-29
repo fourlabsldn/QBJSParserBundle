1e5f45b228a517987493dfa02c15673dd770b975
Luis Galeas <galeaspablo@hotmail.com>
2016-11-29 13:13:29 +0000

    New version of qbjs-parser, embeddables inside embeddables

--- Service/JsonQueryParser.php
+++ Service/JsonQueryParser.php
@@ -9,46 +9,6 @@ use FL\QBJSParser\Serializer\JsonDeserializer;
 class JsonQueryParser
 {
     /**
-     * Class is the $className argument used when constructing @see DoctrineParser
-     * PropertiesMapping is the $fieldsToProperties when constructing @see DoctrineParser.
-     *
-     * @var array
-     */
-    private $classToPropertiesMapping = [];
-
-    /**
-     * Class is the $className argument used when constructing @see DoctrineParser
-     * AssociationMapping is the $fieldPrefixesToClasses when constructing @see DoctrineParser.
-     *
-     * @var array
-     */
-    private $classToAssociationMapping = [];
-
-    /**
-     * Class is the $className argument used when constructing @see DoctrineParser
-     * EmbeddablesPropertiesMapping is the $embeddableFieldsToProperties when constructing @see DoctrineParser.
-     *
-     * @var array
-     */
-    private $classToEmbeddablesPropertiesMapping = [];
-
-    /**
-     * Class is the $className argument used when constructing @see DoctrineParser
-     * EmbeddablesAssociationMapping is the $embeddableFieldPrefixesToClasses when constructing @see DoctrineParser.
-     *
-     * @var array
-     */
-    private $classToEmbeddablesAssociationMapping = [];
-
-    /**
-     * Class is the $className argument used when constructing @see DoctrineParser
-     * EmbeddablesEmbeddableMapping is the $embeddableFieldPrefixesToEmbeddableClasses when constructing @see DoctrineParser.
-     *
-     * @var array
-     */
-    private $classToEmbeddablesEmbeddableMapping = [];
-
-    /**
      * @var DoctrineParser[]
      */
     private $classNameToDoctrineParser = [];
@@ -64,31 +24,43 @@ class JsonQueryParser
      */
     public function __construct(array $classesAndMappings, JsonDeserializer $jsonDeserializer)
     {
+        $classToPropertiesMapping = [];
+        $classToAssociationMapping = [];
+        $classToEmbeddablesPropertiesMapping = [];
+        $classToEmbeddablesInsideEmbeddablesPropertiesMapping = [];
+        $classToEmbeddablesAssociationMapping = [];
+        $classToEmbeddablesEmbeddableMapping = [];
+
         foreach ($classesAndMappings as $classAndMappings) {
+            $class = $classAndMappings['class'];
             foreach ($classAndMappings['properties'] as $field => $entityProperty) {
-                $this->classToPropertiesMapping[$classAndMappings['class']][$field] = $entityProperty ? $entityProperty : $field;
+                $classToPropertiesMapping[$class][$field] = $entityProperty ? $entityProperty : $field;
             }
             foreach ($classAndMappings['association_classes'] as $prefix => $class) {
-                $this->classToAssociationMapping[$classAndMappings['class']][$prefix] = $class;
+                $classToAssociationMapping[$class][$prefix] = $class;
             }
             foreach ($classAndMappings['embeddables_properties'] as $field => $embeddableProperty) {
-                $this->classToEmbeddablesPropertiesMapping[$classAndMappings['class']][$field] = $embeddableProperty ? $embeddableProperty : $field;
+                $classToEmbeddablesPropertiesMapping[$class][$field] = $embeddableProperty ? $embeddableProperty : $field;
+            }
+            foreach ($classAndMappings['embeddables_inside_embeddables_properties'] as $field => $embeddableProperty) {
+                $classToEmbeddablesInsideEmbeddablesPropertiesMapping[$class][$field] = $embeddableProperty ? $embeddableProperty : $field;
             }
             foreach ($classAndMappings['embeddables_association_classes'] as $prefix => $class) {
-                $this->classToEmbeddablesAssociationMapping[$classAndMappings['class']][$prefix] = $class;
+                $classToEmbeddablesAssociationMapping[$class][$prefix] = $class;
             }
             foreach ($classAndMappings['embeddables_embeddable_classes'] as $prefix => $class) {
-                $this->classToEmbeddablesEmbeddableMapping[$classAndMappings['class']][$prefix] = $class;
+                $classToEmbeddablesEmbeddableMapping[$class][$prefix] = $class;
             }
         }
-        foreach ($this->classToPropertiesMapping as $className => $fieldsToProperties) {
+        foreach ($classToPropertiesMapping as $className => $fieldsToProperties) {
             $this->classNameToDoctrineParser[$className] = new DoctrineParser(
                 $className,
                 $fieldsToProperties,
-                $this->classToAssociationMapping[$className] ?? [],
-                $this->classToEmbeddablesPropertiesMapping[$className] ?? [],
-                $this->classToEmbeddablesAssociationMapping[$className] ?? [],
-                $this->classToEmbeddablesEmbeddableMapping[$className] ?? []
+                $classToAssociationMapping[$className] ?? [],
+                $classToEmbeddablesPropertiesMapping[$className] ?? [],
+                $classToEmbeddablesInsideEmbeddablesPropertiesMapping[$className] ?? [],
+                $classToEmbeddablesAssociationMapping[$className] ?? [],
+                $classToEmbeddablesEmbeddableMapping[$className] ?? []
             );
         }
 
