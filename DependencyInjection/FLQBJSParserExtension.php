9be622a5dee9a00d71de9b4ac4af55e8cfd5bffd
Luis Galeas <galeaspablo@hotmail.com>
2016-12-02 11:40:47 +0000

    extension class name change

--- DependencyInjection/FLQBJSParserExtension.php
+++ DependencyInjection/FLQBJSParserExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class QBJSParserExtension extends Extension
+class FLQBJSParserExtension extends Extension
 {
     /**
      * {@inheritdoc}
